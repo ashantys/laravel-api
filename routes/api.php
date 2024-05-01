887818ed215ee3997e7a27642fa1d9d81689be82
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-04-30 18:07:08 -0600

    feat(rutas)se establecieron las rutas de login, signup, logout y user

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'auth'], function(){
+    Route::post('login', 'AuthController@login'); //Se usa para enviar datos
+    Route::post('signup', 'AuthController@signUp');
+
+    Route::group(['middleware => auth:api'], function(){
+        Route::get('logout', 'AuthController@logout'); //Estamos solicitando informacion
+        Route::get('user', 'AuthController@user');
+    });
+});
