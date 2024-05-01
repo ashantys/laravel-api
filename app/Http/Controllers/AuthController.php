4092f3ec46adfe3e230e699f841f1fb37d15ef69
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-05-01 11:54:28 -0600

    feat(post)se creo la migracion para posts

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -63,10 +63,16 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()-toekn()->revoke();
+        $request->user()->token()->revoke();
 
         return response()->json([
             'mesagge' => 'Successfully Logged Out'
         ]);
+    }
+//User method
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
     }
 }
